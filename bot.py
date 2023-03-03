4f3cd2776f965347fea862e057a4b63175201aec
AlexaMusic <119608235+AlexaMusic@users.noreply.github.com>
2023-03-03 14:32:58 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,71 +1,66 @@
 from pyrogram import Client, filters
-from pyrogram.types import Message, Voice
-import asyncio
-import os
-import time
-import ffmpeg
-from pytube import YouTube
-
+from pyrogram.types import Message
+from pymongo import MongoClient
 
 # Create bot instance
-api_id = "8116361"
-api_hash = "3052a25fccbcac402456dd864ed022fd"
-bot_token = "5893160347:AAHfrd5QESVn1twJlt8m2kEEOwIhjponk3g"
-bot = Client(':memory:', api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+bot = Client('5893160347:AAHfrd5QESVn1twJlt8m2kEEOwIhjponk3g')
 
-# Dictionary to store active voice chats
-active_voice_chats = {}
+# Connect to MongoDB
+MONGODB_URL = "mongodb+srv://AlexaMusic:AlexaMusic@cluster0.ev6cioc.mongodb.net/?retryWrites=true&w=majority" # Replace with your MongoDB URL
+client = MongoClient(MONGODB_URL)
+db = client.telegram_bot_db
+banned_users = db.banned_users
 
-# Handle "/play" command
-@bot.on_message(filters.command('play'))
-async def play_song(bot: Client, message: Message):
-    # Check if the message is a reply to another message with a YouTube URL
-    if message.reply_to_message and message.reply_to_message.video:
-        youtube_url = message.reply_to_message.text
-    else:
-        youtube_url = message.text.split(' ', 1)[1]
+# Handle "/ban" command
+@bot.on_message(filters.command('ban'))
+def ban_user(bot: Client, message: Message):
+    user_id = message.text.split(' ')[1] # Get user ID from command argument
+    banned_user = {'user_id': user_id}
+    banned_users.insert_one(banned_user) # Add user to banned users collection
 
-    # Download the video using PyTube
-    video = YouTube(youtube_url)
-    video_stream = video.streams.filter(only_audio=True).first()
-    video_stream.download(output_path='downloads', filename='song')
+    # Ban user in all groups and count the number of groups where the user was banned
+    count = 0
+    for chat in bot.iter_dialogs():
+        if chat.chat.type == 'supergroup':
+            try:
+                bot.kick_chat_member(chat.chat.id, user_id)
+                count += 1
+            except:
+                pass
 
-    # Convert the video to PCM format using ffmpeg
-    input_file = ffmpeg.input('downloads/song.mp4')
-    output_file = ffmpeg.output(input_file, 'pipe:', format='s16le', acodec='pcm_s16le', ac=2, ar='48k')
-    process = output_file.run_async(pipe_stdout=True)
+    message.reply_text(f'{user_id} has been banned in {count} groups.')
 
-    # Start the voice chat and play the song
-    chat_id = message.chat.id
-    voice_chat = await bot.join_chat(chat_id)
-    active_voice_chats[chat_id] = voice_chat
-    while not voice_chat.is_connected:
-        await asyncio.sleep(1)
-    audio_stream = Voice(process.stdout)
-    await voice_chat.play(audio_stream, title=video.title)
+# Handle "/unban" command
+@bot.on_message(filters.command('unban'))
+def unban_user(bot: Client, message: Message):
+    user_id = message.text.split(' ')[1] # Get user ID from command argument
+    banned_users.delete_one({'user_id': user_id}) # Remove user from banned users collection
 
-    # Delete the downloaded video file
-    os.remove('downloads/song.mp4')
+    # Unban user in all groups and count the number of groups where the user was unbanned
+    count = 0
+    for chat in bot.iter_dialogs():
+        if chat.chat.type == 'supergroup':
+            try:
+                bot.unban_chat_member(chat.chat.id, user_id)
+                count += 1
+            except:
+                pass
 
-# Handle "/skip" command
-@bot.on_message(filters.command('skip'))
-async def skip_song(bot: Client, message: Message):
-    chat_id = message.chat.id
-    if chat_id in active_voice_chats:
-        voice_chat = active_voice_chats[chat_id]
-        await voice_chat.stop_playout()
-        message.reply_text('Skipped the current song.')
+    message.reply_text(f'{user_id} has been unbanned in {count} groups.')
 
-# Handle "/end" command
-@bot.on_message(filters.command('end'))
-async def end_song(bot: Client, message: Message):
-    chat_id = message.chat.id
-    if chat_id in active_voice_chats:
-        voice_chat = active_voice_chats[chat_id]
-        await voice_chat.stop_playout()
-        await voice_chat.leave()
-        del active_voice_chats[chat_id]
-        message.reply_text('Ended the song and left the voice chat.')
+# Handle "/broadcast" command
+@bot.on_message(filters.command('broadcast'))
+def broadcast_message(bot: Client, message: Message):
+    text = message.text.split(' ', 1)[1] # Get message text from command argument
 
-# Start the bot
-bot.run()
+    # Send message to all groups and count the number of groups where the message was sent
+    count = 0
+    for chat in bot.iter_dialogs():
+        if chat.chat.type == 'supergroup':
+            try:
+                bot.send_message(chat.chat.id, text)
+                count += 1
+            except:
+                pass
+
+    message.reply_text(f'Message sent to {count} groups.')
